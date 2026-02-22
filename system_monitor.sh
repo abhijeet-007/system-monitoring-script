a2ab9b28bb338dce9826159bcf02ffff719d7ed4
Abhijeet <abhijeetmodak642@gmail.com>
2026-02-22 17:50:50 +0530

    Updated the alert function, now it is working all

--- system_monitor.sh
+++ system_monitor.sh
@@ -9,18 +9,22 @@ BLUE='\033[0;34m'
 NC='\033[0m'
 
 
-DISK_THRESHOLD=10
-MEMORY_THRESHOLD=10
+DISK_THRESHOLD=80
+MEMORY_THRESHOLD=75
 
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-LOG_FILE="$SCRIPT_DIR/system_monitor.log"
+# Log File Location
+LOG_FILE="/home/abhijeet/dec-2025/system_monitor/system-monitoring-script/system_monitor.log"
+
+# Ensure file exists
 touch "$LOG_FILE"
 
+# Clear previous logs
+> "$LOG_FILE"
 
+# Logging Function
 log_alert() {
-    echo "DEBUG: Function running"
-    echo "$(date) - $1" | tee -a "$LOG_FILE"
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> "$LOG_FILE"
 }
 
 #DISK USAGE CODE
@@ -39,11 +43,13 @@ DISK_USAGE=$(df -h | awk 'NR==2 {print $5}' | sed 's/%//')
 echo "Current disk usage: $DISK_USAGE%"
 
 if [ "$DISK_USAGE" -gt "$DISK_THRESHOLD" ]; then
+#MSG="Disk usage exceeded: $DISK_USAGE%"
+   #log_alert "$MSG"
         echo -e ${RED}"WARNING: DISK usage exceeded: ($DISK_USAGE%)${NC}"
-	log_alert "WARNING: DISK usage exceeded: ($DISK_USAGE%)"
+        log_alert "WARNING: DISK usage exceeded: ($DISK_USAGE%)"
 else
         echo -e ${GREEN}"Disk usage is under control${NC}"
-	log_alert "INFO: Disk usage normal ($DISK_USAGE%)"
+        log_alert "INFO: Disk usage normal ($DISK_USAGE%)"
 fi
 echo ""
 
@@ -57,11 +63,13 @@ echo -e ${YELLOW}"Checking memory usage...${NC}"
 MEMORY_USAGE=$(free | awk 'NR==2 {printf("%.0f"), $3/$2 * 100}')
 echo "Current memory usage: $MEMORY_USAGE"
 if [ "$MEMORY_USAGE" -gt "$MEMORY_THRESHOLD" ]; then
-	echo -e ${RED}"WARNING: MEMORY usage exceeded threshold: $MEMORY_USAGE${NC}"
-	log_alert "WARNING: Memory usage exceeded ($MEMORY_USAGE%)"
+#MSG="Memory usage exceeded: $MEMORY_USAGE%"
+   # log_alert "$MSG"
+        echo -e ${RED}"WARNING: MEMORY usage exceeded threshold: $MEMORY_USAGE${NC}"
+        log_alert "WARNING: Memory usage exceeded ($MEMORY_USAGE%)"
 else
-	echo -e ${GREEN}"Memory usage is under control${NC}"
-	log_alert "INFO: Memory usage normal ($MEMORY_USAGE%)"
+        echo -e ${GREEN}"Memory usage is under control${NC}"
+        log_alert "INFO: Memory usage normal ($MEMORY_USAGE%)"
 fi
 echo ""
 
@@ -69,19 +77,17 @@ echo -e ${BLUE}"Top CPU & Memory Processes${NC}"
 echo "------------------------------"
 
 echo -e ${YELLOW}"Top 5 CPU Consuming Processes:${NC}"
+
 TOP_CPU=$(ps -eo pid,ppid,cmd,%cpu --sort=-%cpu | head -n 6)
 echo "$TOP_CPU"
-log_alert="Top 5 CPU Processes:"
+log_alert "Top 5 CPU Processes:"
 echo "$TOP_CPU" >> "$LOG_FILE"
 
 echo -e ${YELLOW}"Top 5 Memory Consuming Processes:${NC}"
 TOP_MEM=$(ps -eo pid,ppid,cmd,%mem --sort=-%mem | head -n 6)
 echo "$TOP_MEM"
-log_alert="Top 5 Memory usage:"
+log_alert "Top 5 Memory usage:"
 echo "$TOP_MEM" >> "$LOG_FILE"
 
-#echo ""
-#END=$(echo -e ${GREEN}"Monitoring Completed.${NC}")
-#echo "$END >> "$LOG_FILE"
-
-
+echo ""
+echo -e ${GREEN}"Monitoring Completed.${NC}"
